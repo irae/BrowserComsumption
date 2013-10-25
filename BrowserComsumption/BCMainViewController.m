b8990ba38e737987330c69d79464242dadfc9def
Irae <irae@irae.pro.br>
2013-10-25 11:43:24 -0700

    code cleanup

--- BrowserComsumption/BCMainViewController.m
+++ BrowserComsumption/BCMainViewController.m
@@ -14,6 +14,8 @@
 
 @implementation BCMainViewController
 
+#pragma mark Controller lifecycle
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,9 +29,22 @@
 {
     [super viewDidLoad];
     
+    self.navigationItem.title = @"Title";
+    [self loadHTMLIntoWebview];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - initialization
+
+- (void)loadHTMLIntoWebview
+{
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"browser_content" ofType:@"html"];
     NSError *err = nil;
-    
     [self.webview loadHTMLString:[NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&err] baseURL:nil];
     if (err) {
         NSLog(@"%@", err);
@@ -38,8 +53,7 @@
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
